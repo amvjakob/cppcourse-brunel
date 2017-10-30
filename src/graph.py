6d208ba5261d45c4ee67c5983798f6a9d97cbf25
antjak <anthony.jakob@outlook.cok>
2017-10-30 16:42:41 +0100

    Additional info on graphs

--- src/graph.py
+++ src/graph.py
@@ -3,7 +3,7 @@ import pylab as pl
 
 raw_data = np.genfromtxt('spikes.gdf')
 
-n_neurons = 400
+n_neurons = 100
 
 select = np.array([d for d in raw_data if d[1] < n_neurons])
 data = select.transpose()
@@ -13,11 +13,12 @@ fig = pl.figure()
 main_scatter = fig.add_subplot(211)
 main_scatter.scatter(0.1 * data[0], data[1], alpha = 0.8, edgecolors = 'none', s = 5)
 main_scatter.set_ylabel('Neuron ID')
+# pl.xlim([200, 1000])
 
 histo = fig.add_subplot(212)
-histo.hist(0.1 * data[0], bins = 100)
+histo.hist(0.1 * data[0], bins = 1000)
 histo.set_xlabel('t [ms]')
 histo.set_ylabel('rate [Hz]')
 
-
+# pl.xlim([200, 1000])
 pl.show()
