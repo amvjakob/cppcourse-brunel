db908576d126adef3819202ef889da661136ebcb
Jakob Anthony Maxime Victor <amjakob@IC-CO-IN-SC064.intranet.epfl.ch>
2017-10-30 14:56:43 +0100

    Graph and histogram

--- src/graph.py
+++ src/graph.py
@@ -1,9 +1,23 @@
 import numpy as np
 import pylab as pl
 
-data = np.genfromtxt('spikes.gdf')
+raw_data = np.genfromtxt('spikes.gdf')
 
-select = np.array([d for d in data if d[1] < 30])
-data1 = select.transpose()
-p1.scatter(0.1 * data[0], data1[1], alpha = 0.8, edgecolors = 'none')
-p1.show()
+n_neurons = 400
+
+select = np.array([d for d in raw_data if d[1] < n_neurons])
+data = select.transpose()
+
+fig = pl.figure()
+
+main_scatter = fig.add_subplot(211)
+main_scatter.scatter(0.1 * data[0], data[1], alpha = 0.8, edgecolors = 'none', s = 5)
+main_scatter.set_ylabel('Neuron ID')
+
+histo = fig.add_subplot(212)
+histo.hist(0.1 * data[0], bins = 100)
+histo.set_xlabel('t [ms]')
+histo.set_ylabel('rate [Hz]')
+
+
+pl.show()
